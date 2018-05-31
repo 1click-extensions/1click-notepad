2d188b270ea7d127fa3ba7d959d54c65218e9e0a
Itsick <any.site.n.site@gmail.com>
2018-06-01 00:43:50 +0300

    css

--- js/notepad.js
+++ js/notepad.js
@@ -20,8 +20,11 @@ notepad = {
         });
         textarea.bind('keydown.text', function(){
             setTimeout(function(){
-                notepad.saveInExtention();
-                if(!title.data('manually setted') && title.val().length < 25){
+                //console.log(title.data('title setted'))
+                if(title.data('title setted')){
+                    notepad.saveInExtention();
+                }
+                if(!title.data('title manually setted') && title.val().length < 25){
                     title.val(textarea.val());
                 }
                 else{
@@ -36,23 +39,39 @@ notepad = {
     },
     addList : function(){
         if(!$('#files-list').length){
-            $('<div id="files-list"></div>').appendTo($('.buttons'));
-
+            $('<div id="files-list"><button type="button" class="chosen"></button><div class="list"></div></div>').appendTo($('.buttons'));
+            $('#files-list .chosen').click(function(){
+                if(!notepad.selectDisabled){
+                    $('#files-list').toggleClass('open');
+                }
+            })
+            $('#files-list').on('click','.remove', function(){
+                notepad.removeFile($(this).next().text());
+                $(this).closest('.part').remove();
+            });
+            $('#files-list').on('click','.text', function(){
+                $('.chosen').text($(this).text())
+                notepad.loadFromStorage($(this).text());
+                $('#files-list').removeClass('open');
+            });
 
         }
+
         notepad.refreshList();
     },
     refreshList : function(){
-        var list = $('#files-list');
-        list.empty().append('<div class="part">' + "Load saved file" +"</div>");
-                list.unbind('change').bind('change', notepad.loadFromStorage);
+        var list = $('#files-list .list'),
+            button = $('<div type="button" class="part"><button class="text">' + "Load saved file" +"</button></div>");
+        list.empty().append(button);
+                //list.unbind('change').bind('change', notepad.loadFromStorage);
         let files = notepad.getAllSaved();
         for(let file of files){
             file = file.replace('item_','');
-            $('#files-list').append('<option value="' + file + '">' + file +"</option>")
+            button.clone().html('<button class="remove">X</button><button class="text">' + file +"</button>").appendTo(list);
         }
-        console.log(" $('#files-list').trigger('change.select2');");
-        $('#files-list').trigger('change.select2');
+        $('#files-list .list .text:first').trigger('click');
+        // console.log(" $('#files-list').trigger('change.select2');");
+        // $('#files-list').trigger('change.select2');
         // $("#files-list").select2({
         //       templateResult: formatState,
         //       change : function(){
@@ -64,16 +83,19 @@ notepad = {
         
     },
     enableList : function(){
-        $('#files-list').attr('disabled','disabled');
+        notepad.selectDisabled = false;
+        $('#files-list').removeClass('disabled');
     },
     diableList : function(){
-        $('#files-list').removeAttr('disabled');
+        notepad.selectDisabled = true;
+        $('#files-list').addClass('disabled');
     },
     onTitleSetted : function(){
-        textarea.unbind('keydown.text');
+        title.data('title setted',1);
+        //textarea.unbind('keydown.text');
     },
     onTitleSettedManually : function(){
-        title.data('manually setted',1);
+        title.data('title manually setted',1);
         notepad.onTitleSetted();
     },
     saveInExtention : function(){
@@ -91,6 +113,9 @@ notepad = {
          link[0].click(); 
          //link.remove();
     },
+    removeFile: function(title){
+        localStorage.removeItem('item_' + title);
+    },
     getFromExtention : function(name){
         return localStorage.getItem('item_'  + name);
     },
@@ -103,10 +128,11 @@ notepad = {
         }
         return collect;
     },
-    loadFromStorage: function (){
-        let load = $('#files-list').val(),
+    selectDisabled: false,
+    loadFromStorage: function (load){
+        let /*oad = $('#files-list').val(),*/
             text = notepad.getFromExtention(load);
-
+        console.log(text);
         if(text){
             title.val(load);
             textarea.val(text);
