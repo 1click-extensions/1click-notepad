1727e4537e0900fdd2b083bbc3fe1f166d745724
Itsick <any.site.n.site@gmail.com>
2018-06-01 12:34:01 +0300

    store file

--- js/notepad.js
+++ js/notepad.js
@@ -10,9 +10,21 @@ notepad = {
 
         notepad.restart();
         notepad.addList();
-        $('.extention').click(notepad.saveInExtention);
-        $('.local').click(notepad.saveToComputer);
-        $('.new').click(notepad.clear);
+        //$('.extention').text(chrome.i18n.getMessage('save_to_local')).click(notepad.saveInExtention);
+        $('.local').text(chrome.i18n.getMessage('save_file')).click(notepad.saveToComputer);
+        $('.new').text(chrome.i18n.getMessage('new_file')).click(notepad.clear);
+        $('#title-label').text(chrome.i18n.getMessage('title_label') + ': ')
+        // $('.request').click(function(){
+        //     chrome.permissions.request({
+        //       permissions: ['unlimitedStorage'],
+        //     }, function(granted) {
+        //       // The callback argument will be true if the user granted the permissions.
+        //       if (granted) {
+        //         notepad.noPermissionAfterRequest = false;
+        //         $('.warning').removeClass('show');
+        //       }
+        //     });
+        // });
     },
     restart : function(){
         title.bind('keydown.text', function(){
@@ -21,7 +33,7 @@ notepad = {
         textarea.bind('keydown.text', function(){
             setTimeout(function(){
                 //console.log(title.data('title setted'))
-                if(title.data('title setted')){
+                if(title.data('title setted') && !notepad.noPermissionAfterRequest){
                     notepad.saveInExtention();
                 }
                 if(!title.data('title manually setted') && title.val().length < 25){
@@ -41,7 +53,7 @@ notepad = {
         if(!$('#files-list').length){
             $('<div id="files-list"><button type="button" class="chosen"></button><div class="list"></div></div>').appendTo($('.buttons'));
             $('#files-list .chosen').click(function(){
-                if(!notepad.selectDisabled){
+                if(1||!notepad.selectDisabled){
                     $('#files-list').toggleClass('open');
                 }
             })
@@ -61,7 +73,7 @@ notepad = {
     },
     refreshList : function(){
         var list = $('#files-list .list'),
-            button = $('<div type="button" class="part"><button class="text">' + "Load saved file" +"</button></div>");
+            button = $('<div type="button" class="part"><button class="text">' + chrome.i18n.getMessage('choose_recent') +"</button></div>");
         list.empty().append(button);
                 //list.unbind('change').bind('change', notepad.loadFromStorage);
         let files = notepad.getAllSaved();
@@ -99,7 +111,31 @@ notepad = {
         notepad.onTitleSetted();
     },
     saveInExtention : function(){
+        // if(JSON.stringify(localStorage).length >= 278){
+        //     chrome.permissions.request({
+        //       permissions: ['unlimitedStorage'],
+        //     }, function(granted) {
+        //       // The callback argument will be true if the user granted the permissions.
+        //       if (granted) {
+        //         notepad._saveInExtention();
+        //       } else {
+        //         notepad.setNoPermissionAfterRequest();
+        //       }
+        //     });
+        // }
+        // else{
+        //     notepad._saveInExtention();
+        // }
+        notepad._saveInExtention();
+        notepad.refreshList();
+        
+    },
+    _saveInExtention : function(){
         localStorage.setItem('item_'  + title.val(), textarea.val());
+    },
+    setNoPermissionAfterRequest: function(){
+        notepad.noPermissionAfterRequest = true;
+        $('.warning').addClass('show');
     },
     saveToComputer : function(){
 
@@ -138,8 +174,28 @@ notepad = {
             textarea.val(text);
             notepad.diableList();
         }
-    }
+    },
+    // addToList : function(name){
 
+    // },
+    // removeFromList : function(name){},
+    // shortList : function(){
+    //     let list = notepad.getJson('items_list');
+    // },
+    // storeJson : function(name, data){
+    //     localStorage.set(name, JSON.stringify(data));
+    // },
+    // getJson : function(name){
+    //     let data = '';
+    //     try{
+    //         data = JSON.parse(localStorage.getItem(name));
+    //     }
+    //     catch(){
+
+    //     }
+    //     return data;
+    // },
+    noPermissionAfterRequest : false
 }
 
 textarea = $('textarea');
